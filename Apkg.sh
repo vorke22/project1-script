6995dba52964736214520b01d7bbacacd93f8814
vorke22 <vokeakpos1@gmail.com>
2022-10-27 21:54:54 -0400

    new

---
+++ Apkg.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+    #Author : Vorke
+    #Date : 27-oct-2022
+
+## ---------- script to install some packages on Alpine -----------------
+
+
+
+sudo apk add  finger -y
+
+sudo apk add  curl -y
+
+sudo apk  add zip -y
+
+sudo apk add  vim -y
+
+sudo apk add  net-tools -y
+
+sudo apk add sysstat -y
+
+sudo apk add wget -y
+
+
+echo "Installation is now completed"
