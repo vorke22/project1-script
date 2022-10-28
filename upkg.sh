8d6fe5d57bb14b7630cfce3745476680aa31df5e
vorke22 <vokeakpos1@gmail.com>
2022-10-27 21:48:44 -0400

    new

---
+++ upkg.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+    #Author : Vorke
+    #Date : 27-oct-2022
+
+## ---------- script to install some packages on ubuntu -----------------
+
+
+
+sudo apt install finger -y
+
+sudo apt install curl -y
+
+sudo apt install zip -y
+
+sudo apt install vim -y
+
+sudo apt install net-tools -ysudo apt install sysstat -y
+
+sudo apt install wget -y
+
+
+echo "Installation is now completed"
+
